0f6b6b9009a3262531f7d6e6236a8ee816541b6d
JustTory <johnnynathany@gmail.com>
2021-05-28 20:04:11 +0700

    post template UI

--- index.php
+++ index.php
@@ -24,8 +24,9 @@
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="profile.php"><i class="bi bi-person mr-2"></i>Profile</a>
+            <a class="dropdown-item" href="createpost.php"><i class="bi bi-pencil-square mr-2"></i>Create post</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="signout.php"><i class="bi bi-box-arrow-right mr-2"></i>Sign out</a>
+            <a class="dropdown-item text-danger" href="signout.php"><i class="bi bi-box-arrow-right mr-2"></i>Sign out</a>
           </div>
         </li>
       <?php else: ?>
@@ -52,17 +53,60 @@
 <div class="container main-cont">
   
   <div class="create-post">
-
+    <div class="row my-3">
+      <div class="col-md-8 offset-md-2">
+        <div class="card">
+          <div class="card-body">
+            <div class="create-form d-flex">
+              <div class="avatar-wrapper d-flex justify-content-center align-items-center">
+                <img class="avatar mr-3" src="images\defaultUserAvatar.png" alt="">
+              </div>
+              <form class="w-100">
+                <div class="form-group m-0">
+                  <input type="text" class="form-control" placeholder="<?php
+                    if($_SESSION['signedIn'] == true) {
+                      echo htmlspecialchars($_SESSION['username']) . ", t";
+                    } else echo "T";
+                  ?>ell us what are you up to?">
+                </div>
+              </form>
+              <div class="embedded d-flex justify-content-center align-items-center">
+                <a class="text-dark" href=""><i class="bi bi-image fa-lg ml-2 mr-2"></i></a>
+                <a class="text-dark" href=""><i class="bi bi-link-45deg fa-lg"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
   <div class="posts">
+
     <div class="row my-3">
       <div class="col-md-8 offset-md-2">
-        <div class="card" style="width: 100%;">
-          <img class="card-img-top" src="images/loginbg.jpg" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="card post">
+          <img class="card-img-top post-img" src="images/loginbg.jpg" alt="Card image cap">
+          <div class="card-body post-body pb-2">
+            <h5 class="card-titl post-title">Card title</h5>
+            <p class="card-text post-content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a class="text-dark font-weight-bold" href="#">
+              <img class="avatar mr-2" src="images\defaultUserAvatar.png" alt="">
+              <?php echo htmlspecialchars("Author") ?>
+            </a>
+            <hr class="mb-2">
+            <div class="interaction">
+              <div class="row">
+                <a class="text-dark col-md-6 text-center" href="#">
+                  <i class="bi bi-hand-thumbs-up"></i>
+                  Like   
+                </a> 
+                <a class="text-dark col-md-6 text-center" href="#">
+                  <i class="bi bi-chat-left"></i>
+                  Comment     
+                </a> 
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -70,12 +114,28 @@
 
     <div class="row my-3">
       <div class="col-md-8 offset-md-2">
-        <div class="card" style="width: 100%;">
-          <img class="card-img-top" src="images/loginbg.jpg" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="card post">
+          <img class="card-img-top post-img" src="images/loginbg.jpg" alt="Card image cap">
+          <div class="card-body post-body pb-2">
+            <h5 class="card-titl post-title">Card title</h5>
+            <p class="card-text post-content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a class="text-dark font-weight-bold" href="#">
+              <img class="avatar mr-2" src="images\defaultUserAvatar.png" alt="">
+              <?php echo htmlspecialchars("Author") ?>
+            </a>
+            <hr class="mb-2">
+            <div class="interaction">
+              <div class="row">
+                <a class="text-dark col-md-6 text-center" href="#">
+                  <i class="bi bi-hand-thumbs-up"></i>
+                  Like   
+                </a> 
+                <a class="text-dark col-md-6 text-center" href="#">
+                  <i class="bi bi-chat-left"></i>
+                  Comment     
+                </a> 
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -83,12 +143,28 @@
 
     <div class="row my-3">
       <div class="col-md-8 offset-md-2">
-        <div class="card" style="width: 100%;">
-          <img class="card-img-top" src="images/loginbg.jpg" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="card post">
+          <img class="card-img-top post-img" src="images/loginbg.jpg" alt="Card image cap">
+          <div class="card-body post-body pb-2">
+            <h5 class="card-titl post-title">Card title</h5>
+            <p class="card-text post-content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a class="text-dark font-weight-bold" href="#">
+              <img class="avatar mr-2" src="images\defaultUserAvatar.png" alt="">
+              <?php echo htmlspecialchars("Author") ?>
+            </a>
+            <hr class="mb-2">
+            <div class="interaction">
+              <div class="row">
+                <a class="text-dark col-md-6 text-center" href="#">
+                  <i class="bi bi-hand-thumbs-up"></i>
+                  Like   
+                </a> 
+                <a class="text-dark col-md-6 text-center" href="#">
+                  <i class="bi bi-chat-left"></i>
+                  Comment     
+                </a> 
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- signin.php
+++ signin.php
@@ -29,12 +29,12 @@
 	<div class="form-container sign-in-container">
 		<form action="signin.php" method="post">
 			<h1>Sign in</h1>
-			<div class="social-container">
+			<div class="social-container mt-4">
 				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
 				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div>
-			<span>or use your account</span>
+			<span>or use your Belogy account</span>
 
 			<input type="email" class="mb-0 <?php 
                     if(isset($errorsSignIn['email'])) echo htmlspecialchars("border-error");
@@ -64,12 +64,12 @@
 		<div class="overlay">
 			<div class="overlay-panel overlay-left">
 				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
+				<p>Time to get back to blogging, please login with your personal info</p>
 				<button class="ghost" id="signIn">Sign In</button>
 			</div>
 			<div class="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
+				<h1>Welcome To Belogy!</h1>
+				<p>Fill in some personal details and start blogging with us</p>
 				<button class="ghost" id="signUp">Sign Up</button>
 			</div>
 		</div>
