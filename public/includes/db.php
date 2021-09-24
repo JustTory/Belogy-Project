0ccb3dae6155a30816587b979bc352b1fc78863a
JustTory <johnnynathany@gmail.com>
2021-09-24 20:06:11 +0700

    changes db.php

--- public/includes/db.php
+++ public/includes/db.php
@@ -1,12 +1,13 @@
 <?php
-    $url = parse_url(getenv("mysql://bcc5dcffb5aa63:47f36dfc@us-cdbr-east-04.cleardb.com/heroku_ac967c3a31d7ed8?reconnect=true"));
+    //$url = parse_url(getenv("mysql://bcc5dcffb5aa63:47f36dfc@us-cdbr-east-04.cleardb.com/heroku_ac967c3a31d7ed8?reconnect=true"));
 
-    $server = $url["us-cdbr-east-04.cleardb.com"];
-    $username = $url["bcc5dcffb5aa63"];
-    $password = $url["47f36dfc"];
-    $db = substr($url["heroku_ac967c3a31d7ed8"], 1);
+    $server = "us-cdbr-east-04.cleardb.com";
+    $username = "bcc5dcffb5aa63";
+    $password = "47f36dfc";
+    $db = "heroku_ac967c3a31d7ed8";
 
     $conn = new mysqli($server, $username, $password, $db);
+    
     if ($mysqli -> connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
         exit();
