011ca32a19de92a678ed7efe328b2c650b710956
JustTory <johnnynathany@gmail.com>
2021-09-24 20:51:33 +0700

    Update db.php

--- public/includes/db.php
+++ public/includes/db.php
@@ -1,15 +1,8 @@
 <?php
-    //$url = parse_url(getenv("mysql://bcc5dcffb5aa63:47f36dfc@us-cdbr-east-04.cleardb.com/heroku_ac967c3a31d7ed8?reconnect=true"));
-
     $server = "us-cdbr-east-04.cleardb.com";
     $username = "bcc5dcffb5aa63";
     $password = "47f36dfc";
     $db = "heroku_ac967c3a31d7ed8";
 
     $conn = new mysqli($server, $username, $password, $db);
-
-    if ($conn->connect_errno) {
-        echo "Failed to connect to MySQL";
-        exit();
-    }
 ?>
